d680f5b2ca5bdbdd80eaeacf60fb4833a7970e22
uwepi514 <uwepi514@gmail.com>
2022-04-28 16:29:51 -0700

    v1

---
+++ iris_v2.R
@@ -0,0 +1,23 @@
+
+## load tidyverse
+library(tidyverse)
+
+
+## plot iris
+
+
+
+
+## iris petal linear regression
+
+iris_petal_reg <- lm(Sepal.Length ~ Sepal.Width, iris)
+
+iris_petal_reg
+
+
+
+str(iris) 
+
+head(iris)
+
+# subset iris petal under 3 
