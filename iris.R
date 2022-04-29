557cdbf2e22f25ad195b46b1171d29dd31713b03
uwepi514 <uwepi514@gmail.com>
2022-04-28 19:19:05 -0700

    q3

--- iris.R
+++ iris.R
@@ -1,58 +1,9 @@
 
-<<<<<<< HEAD
-import data
-=======
-###load data 
-
->>>>>>> 2df3eb17e1e093f90892afe296730150543e76fd
-
-## load data
-
-
-iris <- iris
-
-
-dim(iris)
-
-
-
-## plot data 
-plot(iris$Sepal.Length, iris$Sepal.Width, 
-     xlab="Sepal Length", ylab="Sepal Width", 
-     main="Sepal Length vs. Sepal Width")
-
-
-## data summary 
-str(iris)
-
-
-# plot
-plot(iris)
-
-test
-
-## Student 1 
-
-
-## Student 2 
-
-
-## install use this 
-
-
-install.packages('usethis')
-
-library(usethis)
 
 
 
 
 
 
-
-
-
-
-
-
+#Student 1 code 
 
